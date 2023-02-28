f8a9fd7103f2c11498c4d33ef7fd18e4a9d7963b
MeMosnta <39033905+MeMosnta@users.noreply.github.com>
2023-02-28 11:23:13 +0000

    Update day10.py

--- AdventOfCodePython/day10.py
+++ AdventOfCodePython/day10.py
@@ -16,6 +16,8 @@ At the start of the second cycle, the addx 3 instruction begins execution. Durin
 During the third cycle, X is still 1. After the third cycle, the addx 3 instruction finishes execution, setting X to 4.
 At the start of the fourth cycle, the addx -5 instruction begins execution. During the fourth cycle, X is still 4.
 During the fifth cycle, X is still 4. After the fifth cycle, the addx -5 instruction finishes execution, setting X to -1.
+
+Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
 '''
 def xvalue(lines,i):
     add = []
