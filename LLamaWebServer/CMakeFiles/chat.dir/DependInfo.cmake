6617bdddd83f8a1dfe3d8af2dcd5321c41c921d5
MeMosnta <39033905+MeMosnta@users.noreply.github.com>
2023-04-17 11:39:06 +0100

    Add files via upload

---
+++ LLamaWebServer/CMakeFiles/chat.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lcanham/alpaca copy.cpp/chat.cpp" "CMakeFiles/chat.dir/chat.cpp.o" "gcc" "CMakeFiles/chat.dir/chat.cpp.o.d"
+  "/Users/lcanham/alpaca copy.cpp/utils.cpp" "CMakeFiles/chat.dir/utils.cpp.o" "gcc" "CMakeFiles/chat.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LLamaWebServer/CMakeFiles/chat.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat.dir/chat.cpp.o"
+  "CMakeFiles/chat.dir/chat.cpp.o.d"
+  "CMakeFiles/chat.dir/utils.cpp.o"
+  "CMakeFiles/chat.dir/utils.cpp.o.d"
+  "chat"
+  "chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
